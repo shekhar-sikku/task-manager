ba7468a5c35a102091ce51a537d5ff0d0c8b7a24
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-01-05 09:52:16 +0530

    Image configuration updated in next.config.js file.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+    // domains: ["localhost", "https://img.clerk.com", "img.clerk.com"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '**'
+      },
+    ]
   },
 };
 
